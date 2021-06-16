19414d3b14db48cdfb3f21655fea6367c47a73a6
Peter MoDavis <pmodavis.webdev@gmail.com>
2021-06-16 14:37:33 -0400

    added passoport code as well as getting oauth google clientid and secret

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
+//CLIENT ID: 346916511886-5jr0ul8j3lt9c5q4kmhqujhn3ajhef85.apps.googleusercontent.com
+//CLIENT SECRET: etYffbw679BTNLOpbkonXeHT
+
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oath20").Strategy;
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000;
 
