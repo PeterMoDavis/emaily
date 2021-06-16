d2621421be68cd2ba29dcfdf249d5aa0e56c902d
Peter MoDavis <pmodavis.webdev@gmail.com>
2021-06-16 14:38:19 -0400

    forgot to save index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,3 @@
-//CLIENT ID: 346916511886-5jr0ul8j3lt9c5q4kmhqujhn3ajhef85.apps.googleusercontent.com
-//CLIENT SECRET: etYffbw679BTNLOpbkonXeHT
-
 const express = require("express");
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oath20").Strategy;
